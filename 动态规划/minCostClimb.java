471f7b2833adf13b5f2cac026ebacb6435ab2c46
Merediii <realmadridivy@126.com>
2019-01-05 15:04:08 +0800

    198	打家劫舍

--- 动态规划/minCostClimb.java
+++ 动态规划/minCostClimb.java
@@ -43,16 +43,49 @@ class Solution {
 public class minCostClimb {
     private static int[] r;
 
-    public int minCostClimbingStairs(int[] cost) {
+    public static int f1(int[] cost) {
         r = new int[cost.length+1];
         Arrays.fill(r,-1);
         return f(cost.length,cost);
     }
 
-    public int f(int i,int[] n){
+    public static int f(int i,int[] n){
         if(i==0|| i ==1) return 0;
         if(r[i] >= 0) return r[i];
         r[i] = Math.min(f(i-1,n)+n[i-1],f(i-2,n)+n[i-2]);//记忆化搜索,自顶向下
         return r[i];
+    }
+
+
+    //以下两个都是用的自底向上的递推
+    public static int f2(int[] cost){
+        r = new int[cost.length+1];
+        r[0] = 0;
+        r[1] = 0;
+        for (int i = 2; i <= cost.length; i++)
+            r[i] = Math.min(cost[i-2] + r[i-2],cost[i-1]+r[i-1]);   //不同于f3(),r[i]记录的是到达第i阶的代价,故由于到第i阶台阶,有从第i-2阶和第i-1阶过来这两种,
+                                                                    //因此,r[i-2]+cost[i-2]就是到达第i-2阶台阶再加上cost[i-2]离开第i-2阶到达第i阶的代价,
+                                                                    //同理cost[i-1]+r[i-1]是....i-1......i-1......到达第i阶的代价,取二者中的小者即可
+        return r[cost.length];
+    }
+
+    public static int f3(int[] cost) {
+        int len = cost.length;
+        for(int i=2;i<len;i++){
+            cost[i] = Math.min(cost[i-1]+cost[i],cost[i]+cost[i-2]);//这个计算的是离开第i阶台阶的代价,就是用cost数组来避免新开一个,比如上面f2()中的r[]
+        }
+        return Math.min(cost[len-1],cost[len-2]);   //因为到第i阶台阶,有从第i-2阶和第i-1阶过来这两种,
+                                                    //故由于cost[i]已经代表的是离开第i阶台阶的代价,
+                                                    //所以只需要取cost[i-1]和cost[i-2]之间的小者就可以了,即从第i-2阶离开到第i阶或第i阶离开到第i阶
+    }
+
+    //造成f2()和f3()的递推形式不同的主要原因是,f2()中新开辟的r[i]记录的是到达第i阶的代价(不计算cost[i])
+    //                                    ,而f3()中用cost[i]计算的是离开第i阶台阶的代价(计算cost[i])
+
+    public static void main(String[] args) {
+        int[] test = new int[]{1, 2, 32, 13, 34, 72, 45, 37, 44, 1, 63, 62, 9, 73, 5, 4, 2, 1};
+        System.out.println(f1(test));
+        System.out.println(f2(test));
+        System.out.println(f3(test));
     }
 }
