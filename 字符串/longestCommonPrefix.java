1c88424f0a2fbfaa659e748b1c29593794c834d0
Merediii <realmadridivy@126.com>
2019-01-25 12:31:29 +0800

    14 最长公共前缀

--- 字符串/longestCommonPrefix.java
+++ 字符串/longestCommonPrefix.java
@@ -1,4 +1,5 @@
 package 字符串;
+
 /*
 编写一个函数来查找字符串数组中的最长公共前缀。
 
@@ -17,7 +18,8 @@ package 字符串;
 
 所有输入只包含小写字母 a-z 。
 
-1、所求的最长公共前缀子串一定是每个字符串的前缀子串。所以随便选择一个字符串作为标准，把它的前缀串，与其他所有字符串进行判断，看是否是它们所有人的前缀子串。这里的时间性能是O(m*n*m)。
+1、所求的最长公共前缀子串一定是每个字符串的前缀子串。所以随便选择一个字符串作为标准，把它的前缀串，与其他所有字符串进行判断，
+看是否是它们所有人的前缀子串。这里的时间性能是O(m*n*m)。
 
 2、列出所有的字符串的前缀子串，将它们合并后排序，找出其中个数为n且最长的子串。时间性能为O(n*m+m*n*log(m*n))
 
@@ -30,27 +32,27 @@ package 字符串;
  */
 public class longestCommonPrefix {
     public static String f(String[] strs) {
-        int len = 0x7fffffff,a=0,b=0;
+        int len = 0x7fffffff, a = 0, b = 0;
         for (int i = 0; i < strs.length; i++) {
             len = Math.min(len, strs[i].length());
         }
         for (int i = 0; i < len; i++) {
             for (int j = 1; j < strs.length; j++) {
-                if(strs[j].charAt(i) == strs[0].charAt(i)) continue;
+                if (strs[j].charAt(i) == strs[0].charAt(i)) continue;
                 else
-                    b=1;break;
-
+                    b = 1;
+                break;
             }
-            if(b==1)
-                if(i == 0) return "";
+            if (b == 1)
+                if (i == 0) return "";
                 else
                     break;
             a++;
         }
-        return strs[0].substring(0,a);
+        return strs[0].substring(0, a);
     }
 
-    public static void main(String[] args){
-        System.out.println(f(new String[]{"flower","flow","flight"}));
+    public static void main(String[] args) {
+        System.out.println(f(new String[]{"flower", "flow", "flight"}));
     }
 }
