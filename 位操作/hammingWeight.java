13ac8c573e2f58dd9b98ce0251b7c472735a3749
merediii <realmadridivy@126.com>
2020-02-03 16:10:53 +0800

    392. 判断子序列

--- 位操作/hammingWeight.java
+++ 位操作/hammingWeight.java
@@ -33,16 +33,16 @@ public class hammingWeight {
     private static Scanner sc = new Scanner(System.in);
 
     public static int f(int n) {
-        if(n == 0 )return 0;
+        if (n == 0) return 0;
         int count = 0;
-        while (n != 0){
-            count+=n&1;     //只有 1 & 1 = 1
+        while (n != 0) {
+            count += n & 1;     //只有 1 & 1 = 1
             n >>>= 1;
         }
         return count;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(f(sc.nextInt()));
     }
 }
