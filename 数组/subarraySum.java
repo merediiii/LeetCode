08bb0d4a11771f0a726376843a959cb6ac63fdea
Merediii <realmadridivy@126.com>
2020-05-17 15:27:54 +0800

    560. 和为K的子数组

--- 数组/subarraySum.java
+++ 数组/subarraySum.java
@@ -3,7 +3,7 @@ package 数组;
 import java.util.HashMap;
 
 /*
-
+560. 和为K的子数组
 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
 
 示例 1 :
@@ -48,18 +48,46 @@ public class subarraySum {
     }
 
     //O(n)
-    public static int g(int[] nums, int k){
-        int left = 0,right = 1, count = 0,ans = 0 ;
-        int[] temp = new int[nums.length+1];
-        for (int i = 0; i < nums.length; i++) {
-            
+    public static int g(int[] nums, int k) {
+        int len = nums.length, count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);                                      //此处表示前缀和为0，且用于标识和等于本身的元素
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        
+        return count;
     }
 
+    public static int k(int[] nums, int k) {
+        if (nums.length == 0) return 0;
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+            max = max > nums[i] ? max : nums[i];
+            min = min < nums[i] ? min : nums[i];
+        }
+        int count = 0;
+        int map[] = new int[max - min + 1];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == k) {
+                count++;
+            }
+            if (nums[i] - k >= min && nums[i] - k <= max) {
+                count += map[nums[i] - k - min];
+            }
+            map[nums[i] - min]++;
+        }
+        return count;
+
+    }
 
     public static void main(String[] args) {
-        System.out.println(f(new int[]{1,2,3,4,5,6,7,1,23,21,3,1,2,1,1,1,1,1,12,2,3,2,3,2,2}, 1));
+        System.out.println(k(new int[]{1, 1, -1, 0, 3, 2, -1, 2, 1}, 2));
+
     }
 }
 
